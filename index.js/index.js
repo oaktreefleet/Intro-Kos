40687fcfda04e1beff48d51b33e83aae6fd8eb6d
oaktreefleet <oaktreefleet@yahoo.com>
2023-02-10 06:24:18 -0700

    my first js/html push

---
+++ index.js/index.js
@@ -0,0 +1,25 @@
+var bankAccountBalance = 52.10;
+
+var firstName = "Tom";
+var lastName = 'Sawyer';
+
+var numberOfFriends = 5;
+
+var isHotOutside = false;
+var isHappy = true;
+
+console.log('Thank You God!');
+
+console.log(5 + 3);
+console.log(4 - 2);
+console.log(6 * 7);
+console.log(8 / 2);
+
+console.log(bankAccountBalance - 30);
+console.log(bankAccountBalance * numberOfFriends);
+
+console.log(5 == 5);
+console.log(5 == 6);
+
+console.log(bankAccountBalance == numberOfFriends);
+console.log(bankAccountBalance == 52.10);
